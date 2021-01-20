f8d8db1b77494aff6bfeb35508600756e5db0fc0
Ivan Rudkovskiy <i.rydkovskiy@sotbit.ru>
2021-01-20 15:51:22 +0300

    task1completed

---
+++ task/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sotbit Learn</title>
+</head>
+
+<body>
+<p>Мороз и солнце; день чудесный!<br>
+    Еще ты дремлешь, друг прелестный —<br>
+    Пора, красавица, проснись:<br>
+    Открой сомкнуты негой взоры<br>
+    Навстречу северной Авроры,<br>
+    Звездою севера явись!</p>
+</body>
+</html>
